832ae99fc1141aa825dd9dcee3c8347f4b156c36
Tom Seifert <dev@tpseifert.de>
2022-12-09 09:08:55 +0100

    Set window title

--- src/jvmMain/kotlin/de/syex/dailyupdate/Main.kt
+++ src/jvmMain/kotlin/de/syex/dailyupdate/Main.kt
@@ -4,7 +4,10 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        title = "Daily Update Generator",
+        onCloseRequest = ::exitApplication
+    ) {
         App()
     }
 }
